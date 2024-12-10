3e4bc7a5d76106980322d0ec1206290af5b99ffd
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-10 13:37:47 +0530

    Add files via upload

---
+++ nextlnandnext.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class nextlnandnext {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str1=sc.next();
+        String str2=sc.nextLine();
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+    
+}
