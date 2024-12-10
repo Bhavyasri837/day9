2b14cb98a9c9820a75ea87cc2b463d78a4c84395
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-10 13:41:07 +0530

    Add files via upload

---
+++ splitting.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class splitting {
+    public static  void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+       
+        String split[]=str.split(" ");
+        System.out.println(Arrays.toString(split));
+       ;
+
+    }
+    
+}
