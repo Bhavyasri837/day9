49aabebd371be0ba19ce88fb317ab9aaed9043d9
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-10 10:25:27 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sample
+{
+    public static void main(String args[])
+    {
+        String a="hello";
+        String b=new String("how are you");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
